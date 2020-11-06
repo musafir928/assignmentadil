662b44e9fc76319a1fc9b90808bf842ece491963
Adil <uganoghli@gmail.com>
2020-11-06 08:40:05 +0100

    add --fix option

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,6 +71,21 @@ export default {
   ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
   */
-  build: {
+
+    build: {
+      extend(config, { isDev, isClient }) {
+        if (isDev && isClient) {
+          config.module.rules.push({
+            enforce: 'pre',
+            test: /\.(js|vue)$/,
+            loader: 'eslint-loader',
+            exclude: /(node_modules)/,
+            options:{
+              fix: true
+            }
+          })
+        }
+      }
+    }
   }
-}
+
